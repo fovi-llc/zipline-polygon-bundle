0433987f76dadd85f15caacc308f069784de194c
Jim White <jim@ifcx.org>
2024-07-18 22:16:08 -0700

    Renamed package and repo.

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,11 @@
 from tickers_and_names import load_all_tickers
 from config import *
-from zipline_bundle_polygon import register_polygon_equities_bundle
+from zipline_polygon_bundle import register_polygon_equities_bundle
 
 __all__ = [
     "get_data_dir",
     "get_flat_files_dir",
     "get_asset_files_dir",
-    "get_minute_aggs_dir"
-    "load_all_tickers",
+    "get_minute_aggs_dir" "load_all_tickers",
     "register_polygon_equities_bundle",
 ]
