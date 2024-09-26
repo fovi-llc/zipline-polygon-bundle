17f564fd206b9c2668dc435a99dd735735e832f8
Jim White <jim@ifcx.org>
2024-09-26 15:16:52 -0700

    Use Poetry

--- tests/test_polygon_file_reader.py
+++ tests/test_polygon_file_reader.py
@@ -2,22 +2,35 @@ import glob
 import os
 import unittest
 import pandas as pd
-from polygon_file_reader import convert_timestamp, convert_minute_csv_to_parquet, process_all_minute_csv_to_parquet
+from polygon_file_reader import convert_timestamp
 
 
 class TestPolygonFileReader(unittest.TestCase):
 
     def test_convert_timestamp(self):
         # Test converting a timestamp string to a pandas Timestamp object
-        self.assertEqual(convert_timestamp("0"),                pd.Timestamp("1970-01-01 00:00:00"))
-        self.assertEqual(convert_timestamp("0000000000000000"), pd.Timestamp("1970-01-01 00:00:00"))
-        self.assertEqual(convert_timestamp("00000000000001"),   pd.Timestamp("1970-01-01 00:00:01"))
-        self.assertEqual(convert_timestamp("1000000001"),       pd.Timestamp("2001-09-09 01:46:41"))
-        self.assertEqual(convert_timestamp("5000000000"),       pd.Timestamp("2128-06-11 08:53:20"))
+        self.assertEqual(convert_timestamp("0"), pd.Timestamp("1970-01-01 00:00:00"))
+        self.assertEqual(
+            convert_timestamp("0000000000000000"), pd.Timestamp("1970-01-01 00:00:00")
+        )
+        self.assertEqual(
+            convert_timestamp("00000000000001"), pd.Timestamp("1970-01-01 00:00:01")
+        )
+        self.assertEqual(
+            convert_timestamp("1000000001"), pd.Timestamp("2001-09-09 01:46:41")
+        )
+        self.assertEqual(
+            convert_timestamp("5000000000"), pd.Timestamp("2128-06-11 08:53:20")
+        )
         # 10_000_000_000 is the threshold for milliseconds
-        self.assertEqual(convert_timestamp("10000000001"),      pd.Timestamp("1970-04-26 17:46:40.001000"))
+        self.assertEqual(
+            convert_timestamp("10000000001"), pd.Timestamp("1970-04-26 17:46:40.001000")
+        )
         # 100_000_000_000_000 is the threshold for nanoseconds
-        self.assertEqual(convert_timestamp("100000000000001"),  pd.Timestamp("1970-01-02 03:46:40.000000001"))
+        self.assertEqual(
+            convert_timestamp("100000000000001"),
+            pd.Timestamp("1970-01-02 03:46:40.000000001"),
+        )
 
     # def test_convert_minute_csv_to_parquet(self):
     #     # Test converting a minute-level CSV file to Parquet format
