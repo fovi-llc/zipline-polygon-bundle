7a7b47dbe8f117c8fa4a68c9e2e404248b32358b
Jim White <jim@ifcx.org>
2025-03-09 13:35:03 -0700

    Fix up the bad aggs file (easier than changing aggregation to fix window_start when it is wrong and this is the only file, we'll still warnings when the data is bad).

---
+++ fix_2019-08-12.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the file path
+file_path=$POLYGON_DATA_DIR/flatfiles/us_stocks_sip/day_aggs_v1/2019/08/2019-08-12.csv.gz
+
+# Define the incorrect and correct timestamps
+incorrect_timestamp="1565668800000000000"
+correct_timestamp="1565582400000000000"
+
+# Save the bad old file
+SAVE_DIR=$POLYGON_DATA_DIR/flatfiles/us_stocks_sip/day_aggs_v1/saved
+mkdir -p $SAVE_DIR
+bad_file=$SAVE_DIR/2019-08-12.csv.gz
+
+mv $file_path $bad_file
+
+# Unzip, replace the incorrect timestamp, and zip back
+gunzip -c "$bad_file" | sed "s/$incorrect_timestamp/$correct_timestamp/g" | gzip -c > "$file_path"
+
+# aws s3 sync s3://flatfiles/us_stocks_sip/day_aggs_v1 \
+#    $POLYGON_DATA_DIR/flatfiles/us_stocks_sip/day_aggs_v1 \
+#    --checksum-mode ENABLED --endpoint-url https://files.polygon.io;
